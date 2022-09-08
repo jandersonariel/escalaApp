f6315ed4b201dca85aa8ba8f6ad9af627f0fbb97
janderson <janderson.ariel@yahoo.com.br>
2022-09-08 14:21:54 -0300

    att

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import Home from './src/screens/Home';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
