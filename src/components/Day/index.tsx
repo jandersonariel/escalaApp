85513339da64e9eda5d0993442ac88a84234548d
janderson <janderson.ariel@yahoo.com.br>
2022-09-15 13:48:05 -0300

    org

---
+++ src/components/Day/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Day = () => {
+  return (
+    <>
+
+    </>
+  )
+};
+
+export default Day;
--- src/screens/calendar/index.tsx
+++ src/screens/calendar/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Calendar = () => {
-  return <div>aqui o calendario da semana</div>;
+  return (
+    <div>
+      aqui o calendario da semana
+    </div>
+  )
 };
 
 export default Calendar;
