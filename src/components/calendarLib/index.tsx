467dba230fbb918faff930a923c48d063c522b0c
janderson <janderson.ariel@yahoo.com.br>
2022-10-04 00:21:37 -0300

    test

--- src/components/calendarLib/index.tsx
+++ src/components/calendarLib/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 
@@ -7,7 +8,18 @@ const Calendarlibb = () => {
   return (
     <>
       <Calendar
-        style={{ height: "80%", width: "100%" }}
+        style={{ height: "80%" }}
+        onDayLongPress={(e) => {
+          console.log(e)
+        }}
+      //dayComponent={(e) => {
+      //console.log(e)
+      //return (
+      //<View style={{ height: 40, width: 40, backgroundColor: "aqua" }} >
+      // <Text>{e.date.day}</Text>
+      // </View>
+      // )
+      // }}
       />
 
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,7 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  display: flex;
   justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background: ${({theme} ) => theme.colors.background}
 `;
 
 export const Text = styled.Text`
