b212fee87be58b0cd50ed962c06fbbc530a798af
janderson <janderson.ariel@yahoo.com.br>
2022-10-06 11:46:34 -0300

    tradutor

--- src/components/calendarLib/index.tsx
+++ src/components/calendarLib/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
 
+LocaleConfig.locales['pt'] = {
+  monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+  monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+  today: "Hoje",
+  dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"],
+  dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab",],
+}
+LocaleConfig.defaultLocale = "pt"
 
 
 const Calendarlibb = () => {
